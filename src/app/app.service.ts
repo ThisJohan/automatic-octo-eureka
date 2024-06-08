026a5b4124c8ae16b9560486a3a48f32e0ea941c
Peyman Khosravi <this.peyman@gmail.com>
2024-06-08 13:08:10 +0330

    tmp

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+export interface Contact {
+    id: number;
+    birthDate: string;
+    email: string;
+    firstName: string;
+    gender: string;
+    lastName: string;
+    mobileNumber: string;
+  }
+
 @Injectable({providedIn: 'root'})
 export class AppService {
 
@@ -10,5 +20,9 @@ export class AppService {
 
     login(username: string, password: string) {
         return this.http.post(`${this.baseUrl}/Users/login`, {username, password}, {responseType: 'text'});
+    }
+
+    list() {
+        return this.http.get<Contact[]>(`${this.baseUrl}/Contact`);
     }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { AppService, Contact } from '../app.service';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ButtonModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
 
+  contacts: Contact[] = [];
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.appService.list().subscribe((v) => {
+      this.contacts = v;
+    });
+  }
+
+  edit(contact: Contact) {
+    console.log({ contact });
+  }
+
+  delete(contactId: number) {
+    console.log({ contactId });
+  }
 }
